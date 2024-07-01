5eedd26f2edd4e926b471acb973835caa8f4651b
Hans Peter Diesing <diesinghans@gmail.com>
2024-07-01 12:13:38 +0200

    Added Obfuscation

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
-	plugins: [
-		laravel({
+    plugins: [
+        laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/notes.js'
             ],
-			refresh: true,
-		}),
-	],
-	server: {
-		hmr: {
-			host: 'localhost',
-		},
-	},
+            refresh: true,
+        }),
+        obfuscatorPlugin({
+            include: ["resources/js/*.js"],
+            apply: "build",
+            options: {
+                // your javascript-obfuscator options
+                debugProtection: true,
+                // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
+            },
+        }),
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
