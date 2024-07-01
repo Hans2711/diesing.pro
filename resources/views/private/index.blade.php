c77c2d97f80ba0d7b0511da8d8b61aac6acd0de0
Hans Peter Diesing <diesinghans@gmail.com>
2024-07-01 02:01:09 +0200

    Fix

--- resources/views/private/index.blade.php
+++ resources/views/private/index.blade.php
@@ -6,7 +6,15 @@
     @if ($isAuthenticated)
         <p class="text-green-800">Authentifiziert</p>
 
-        @include('private.tools-sidebar', ['active' => ''])
+        <div class="flex flex-col md:flex-row md:justify-between w-full">
+            <div class="order-2 md:order-1 mt-4 md:mt-0 md:mr-4 md:w-auto">
+            </div>
+
+            <div class="order-1 md:order-2 w-full md:w-1/5">
+                @include('private.tools-sidebar', ['active' => 'notes'])
+            </div>
+        </div>
+
     @else
         <p class="text-red-800">Nicht Authentifiziert</p>
         <form method="POST" action="#" class="mt-5">
