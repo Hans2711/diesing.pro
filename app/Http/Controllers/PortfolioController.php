a6443df8f47a9e75fe9a0335f47ecab3bd708a85
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-19 19:39:09 +0200

    Fix Stuff

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,23 +9,18 @@ class PortfolioController extends Controller
     //
     public function list()
     {
-        $projects= [
+        $projects = [
             [
-                'title' => 'Werstener Jonges',
-                'description' => 'Der Heimatverein Werstener Jonges ist seit 1953 in Wersten aktiv. Wir pflegen und fördern lokale Traditionen und die Gemeinschaft. Unsere Aktivitäten umfassen Sommerfeste, Bibliotheksprojekte und verschiedene Veranstaltungen.',
-                'image' => 'resources/portfolio/werstener-jonges/homepage.png',
-                'link' => 'https://werstener-jonges.de/'
-            ],
-            [
-                'title' => 'Hilden Haze',
-                'description' => 'Willkommen bei Hilden Haze! Wir sind ein Verein, der im Rahmen der Legalisierung Cannabis gemeinschaftlich anbaut. Unser Ziel ist es, hochwertiges, biologisches Cannabis anzubieten. Wir streben danach, Hilden Haze zu einer sicheren Quelle für legales Cannabis in der Region zu machen',
-                'image' => 'resources/portfolio/hilden-haze/homepage.png',
-                'link' => 'https://www.hilden-haze.de'
+                "title" => "Werstener Jonges",
+                "description" =>
+                    "Der Heimatverein Werstener Jonges ist seit 1953 in Wersten aktiv. Wir pflegen und fördern lokale Traditionen und die Gemeinschaft. Unsere Aktivitäten umfassen Sommerfeste, Bibliotheksprojekte und verschiedene Veranstaltungen.",
+                "image" => "resources/portfolio/werstener-jonges/homepage.png",
+                "link" => "https://werstener-jonges.de/",
             ],
         ];
 
-        return view('portfolio.list', [
-            'projects' => $projects,
+        return view("portfolio.list", [
+            "projects" => $projects,
         ]);
     }
 }
