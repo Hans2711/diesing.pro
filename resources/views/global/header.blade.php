dc142572278794b6dd421e580321fec23c94ed05
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-19 19:39:09 +0200

    Fix Stuff

--- resources/views/global/header.blade.php
+++ resources/views/global/header.blade.php
@@ -1,10 +1,10 @@
 <header class="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#F4EFE6] px-10 py-3">
     <div class="flex flex-1 justify-end gap-8">
-        <div class="flex items-center gap-5 md:gap-9 flex-wrap">
-            <a class="text-[#1C160C] text-md font-medium leading-normal @if ($active == 'portfolio') active @endif" href="{{(url('/portfolio'))}}">Portfolio</a>
-            <a class="text-[#1C160C] text-md font-medium leading-normal @if ($active == 'contact') active @endif" href="{{url('/kontakt')}}">Kontakt</a>
-            <a class="text-[#1C160C] text-md font-medium leading-normal" href="#">Fotogallerie</a>
-            <a class="text-[#1C160C] text-md font-medium leading-normal @if ($active == 'private') active @endif" href="{{url('/privater-bereich')}}">Privater Bereich</a>
+        <div class="flex items-center gap-3 md:gap-5 flex-wrap">
+            <a class="@if ($active == 'portfolio') active @endif header-button" href="{{ url('/portfolio') }}">Portfolio</a>
+            <a class="@if ($active == 'contact') active @endif header-button" href="{{ url('/kontakt') }}">Kontakt</a>
+            <!-- <a class="header-button" href="#">Fotogallerie</a> -->
+            <a class="@if ($active == 'private') active @endif header-button" href="{{ url('/privater-bereich') }}">Privater Bereich</a>
         </div>
         <a href="/">
             <div
