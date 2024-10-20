56a986f983fdd950870f2c3b61c30076f9f3e6fc
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-20 16:57:08 +0200

    Fix Stuff

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     laravel({
       input: [
         "resources/css/app.css",
+        "resources/css/contact.css",
         "resources/js/app.js",
         "resources/js/notes.js",
         "resources/js/redirects.js",
