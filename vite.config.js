11b848fc8e9f803d1ba01d00884c2862fdbdd3e7
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-20 16:42:31 +0200

    Fix Stuff

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,30 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/notes.js',
-                'resources/js/redirects.js',
-                'resources/js/files.js',
-                'resources/js/parts/password.js',
-            ],
-            refresh: true,
-        }),
-        obfuscatorPlugin({
-            include: [
-                "resources/js/notes.js",
-                "resources/js/redirects.js"
-            ],
-            apply: "build",
-            options: { },
-        }),
-    ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
+  plugins: [
+    laravel({
+      input: [
+        "resources/css/app.css",
+        "resources/js/app.js",
+        "resources/js/notes.js",
+        "resources/js/redirects.js",
+        "resources/js/files.js",
+        "resources/js/parts/password.js",
+        "resources/js/parts/contact.js",
+      ],
+      refresh: true,
+    }),
+    obfuscatorPlugin({
+      include: ["resources/js/notes.js", "resources/js/redirects.js"],
+      apply: "build",
+      options: {},
+    }),
+  ],
+  server: {
+    hmr: {
+      host: "localhost",
     },
+  },
 });
