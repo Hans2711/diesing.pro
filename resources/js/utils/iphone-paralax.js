5f5affd02b6ef490ca03982525a95c88ec1d0947
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-23 18:23:34 +0200

    Fix Paralax

---
+++ resources/js/utils/iphone-paralax.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the device width is less than or equal to 768 pixels
+    if (window.innerWidth <= 768) {
+        window.addEventListener('scroll', function() {
+            const scrollPosition = window.scrollY;
+
+            document.querySelectorAll('.parallax-image').forEach((element) => {
+                const speed = element.getAttribute('data-speed') || 0.1; // Adjust speed if necessary
+                const elementHeight = element.offsetHeight; // Get the height of the element
+
+                // Calculate new background position
+                let newYPosition = -scrollPosition
+
+                // Limit the new Y position to not exceed the image height
+                newYPosition = Math.max(newYPosition, -elementHeight); // Prevent moving higher than image height
+
+                newYPosition = newYPosition * speed; // Adjust speed as needed
+
+                element.style.backgroundPositionY = `${newYPosition}px`;
+            });
+        });
+    }
+});
+
