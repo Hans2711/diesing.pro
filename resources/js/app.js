a413804a7b83c0cbbebe5f0d15804e9ddb8ffa5e
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-23 18:06:24 +0200

    Fix Paralax

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,3 +8,16 @@ import.meta.glob([
 ]);
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the device width is less than or equal to 768 pixels
+    if (window.innerWidth <= 768) {
+        window.addEventListener('scroll', function() {
+            const scrollPosition = window.scrollY;
+
+            document.querySelectorAll('.parallax-image').forEach((element) => {
+                const speed = element.getAttribute('data-speed') || 0.5; // Adjust speed if necessary
+                element.style.backgroundPositionY = `-${scrollPosition * speed}px`;
+            });
+        });
+    }
+});
