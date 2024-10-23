0ec0102503a5fe36de3ae72e7233fc8c03812d56
Hans Peter Diesing <hans.diesing@netigo.de>
2024-10-23 18:20:27 +0200

    Fix Paralax

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
             const scrollPosition = window.scrollY;
 
             document.querySelectorAll('.parallax-image').forEach((element) => {
-                const speed = element.getAttribute('data-speed') || 0.5; // Adjust speed if necessary
-                element.style.backgroundPositionY = `-${scrollPosition * speed}px`;
+                const speed = element.getAttribute('data-speed') || 0.1; // Adjust speed if necessary
+                const elementHeight = element.offsetHeight; // Get the height of the element
+
+                // Calculate new background position
+                let newYPosition = -scrollPosition
+
+                // Limit the new Y position to not exceed the image height
+                newYPosition = Math.max(newYPosition, -elementHeight); // Prevent moving higher than image height
+
+                newYPosition = newYPosition * speed; // Adjust speed as needed
+
+                element.style.backgroundPositionY = `${newYPosition}px`;
             });
         });
     }
