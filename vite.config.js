f74afb58b06a475540f6333d9a1b47aace504fc4
Hans Peter Diesing <hans.diesing@netigo.de>
2024-11-20 00:26:02 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: [
         "resources/css/app.css",
         "resources/css/contact.css",
+        "resources/css/diff-table.css",
         "resources/js/app.js",
         "resources/js/notes.js",
         "resources/js/redirects.js",
