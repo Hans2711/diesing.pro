28b9c55203e298129bea295aff4be2130b062feb
Hans Peter Diesing <hans.diesing@netigo.de>
2024-12-22 06:51:30 +0100

    Attempted Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,38 +4,96 @@
     @vite(['resources/js/utils/iphone-paralax.js'])
     <h1>{{ __('text.welcome') }}</h1>
 
-    <div class="parallax-image" style="background-image: url('{{ Vite::asset('resources/images/kontakt.jpg') }}');">
-        <h2 class="parallax-text text-white md:text-black text-3xl">
-            <a wire:navigate.hover href="{{url(Config::get('app.locale') . '/' . __('url.contact'))}}">
-                {{ __('text.contact') }}
+    <div class="md:columns-2 columns-1 md:mb-8 mb-0" id="portfolio">
+        <!-- Card: Contact -->
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url(Config::get('app.locale') . '/' . __('url.contact')) }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/kontakt.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.contact') }}
+                    </div>
+                </div>
             </a>
-        </h2>
-    </div>
+        </div>
 
-    <div class="parallax-image" style="background-image: url('{{ Vite::asset('resources/images/portfolio.jpg') }}');">
-        <h2 class="parallax-text text-white text-3xl">
-            <a wire:navigate.hover href="{{url(Config::get('app.locale') . '/' . __('url.portfolio'))}}">
-                {{ __('text.portfolio') }}
+        <!-- Card: Portfolio -->
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url(Config::get('app.locale') . '/' . __('url.portfolio')) }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/portfolio.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.portfolio') }}
+                    </div>
+                </div>
             </a>
-        </h2>
+        </div>
     </div>
 
-    <div class="parallax-image" style="background-image: url('{{ Vite::asset('resources/images/private.jpg') }}');">
-        <h2 class="parallax-text text-white text-3xl">
-            <a wire:navigate.hover href="{{url('/privater-bereich')}}">
-                {{ __('text.private-tools') }}
+    <div class="md:columns-2 columns-1 md:mb-8 md-0" id="portfolio">
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url(Config::get('app.locale') . '/' . __('url.tester')) }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/testing.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.tester') }}
+                    </div>
+                </div>
             </a>
-        </h2>
+        </div>
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url(Config::get('app.locale') . '/' . __('url.teams')) }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/random.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.random-teams') }}
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 
-    <div class="parallax-image" style="background-image: url('{{ Vite::asset('resources/images/law.jpg') }}');">
-        <h2 class="parallax-text text-3xl">
-            <a wire:navigate.hover href="{{url(Config::get('app.locale') . '/' . __('url.imprint'))}}"> {{ __('text.imprint') }}</a>
-            {{ __('text.and') }}
-            <a wire:navigate.hover href="{{url(Config::get('app.locale') . '/' . __('url.data-protection'))}}">{{ __('text.data-protection') }}</a>
-        </h2>
+    <div class="md:columns-2 columns-1 md:mb-8 md-0" id="portfolio">
+        <!-- Card: Private Tools -->
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url('/privater-bereich') }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/private.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.private-tools') }}
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- Card: Imprint & Data Protection -->
+        <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
+            <a href="{{ url(Config::get('app.locale') . '/' . __('url.imprint')) }}" wire:navigate.hover >
+                <div class="relative h-0 pb-32">
+                    <img class="absolute top-0 left-0 w-full h-32 object-cover"
+                         src="{{ Vite::asset('resources/images/law.jpg') }}">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">
+                        {{ __('text.imprint') }} {{ __('text.and') }} {{ __('text.data-protection') }}
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
-
-
-
 @endsection
