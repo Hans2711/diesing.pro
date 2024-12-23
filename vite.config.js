1ae5a1a77ba24096041308c533e8d6bd78cddf14
Hans Peter Diesing <hans.diesing@netigo.de>
2024-12-23 18:41:57 +0100

    Attempted Fix

--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,38 @@ import laravel from "laravel-vite-plugin";
 import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        "resources/css/app.css",
-        "resources/css/contact.css",
-        "resources/css/diff-table.css",
-        "resources/js/app.js",
-        "resources/js/notes.js",
-        "resources/js/redirects.js",
-        "resources/js/files.js",
-        "resources/js/parts/password.js",
-        "resources/js/contact.js",
-        "resources/js/utils/iphone-paralax.js",
-      ],
-      refresh: true,
-    }),
-    obfuscatorPlugin({
-      include: [
+    plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/css/contact.css",
+                "resources/css/diff-table.css",
+                "resources/js/app.js",
+                "resources/js/notes.js",
+                "resources/js/redirects.js",
+                "resources/js/files.js",
+                "resources/js/parts/password.js",
+                "resources/js/parts/header.js",
+                "resources/js/contact.js",
+                "resources/js/fingerprinting.js",
+                "resources/js/utils/iphone-paralax.js",
+            ],
+            refresh: true,
+        }),
+        obfuscatorPlugin({
+            include: [
                 "resources/js/notes.js",
                 "resources/js/redirects.js",
                 "resources/js/contact.js",
-                "resources/js/parts/password.js"
+                "resources/js/parts/password.js",
             ],
-      apply: "build",
-      options: {},
-    }),
-  ],
-  server: {
-    hmr: {
-      host: "localhost",
+            apply: "build",
+            options: {},
+        }),
+    ],
+    server: {
+        hmr: {
+            host: "localhost",
+        },
     },
-  },
 });
