92d5bdf6cf90bd70b5e7b6ed12e65289677265c2
Hans Peter Diesing <hans.diesing@netigo.de>
2025-01-07 05:45:03 +0100

    Fix SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,10 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="preload" href="{{ Vite::asset('font/static/FiraCode-Regular.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
-    <link rel="preload" href="{{ Vite::asset('font/static/FiraCode-Medium.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
-    <link rel="preload" href="{{ Vite::asset('font/static/FiraCode-SemiBold.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
-    <link rel="preload" href="{{ Vite::asset('font/static/FiraCode-Bold.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
+    <link rel="preload" href="{{ Vite::asset('resources/font/static/FiraCode-Regular.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
+    <link rel="preload" href="{{ Vite::asset('resources/font/static/FiraCode-Medium.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
+    <link rel="preload" href="{{ Vite::asset('resources/font/static/FiraCode-SemiBold.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
+    <link rel="preload" href="{{ Vite::asset('resources/font/static/FiraCode-Bold.ttf') }}" as="font" type="font/ttf" crossorigin="anonymous">
 
     <title>
     @if (isset($title))
