b2d7c7d1ad3802697975084812c7cb65abebd4c0
Hans Peter Diesing <hans.diesing@netigo.de>
2025-01-11 15:32:02 +0100

    Fix

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app', ['title' => 'Kontakt', 'active' => 'contact'])
 
 @section('content')
-    @vite(['resources/css/contact.css'])
     <h1>{{ __('text.contact') }}</h1>
-
     <livewire:contact-form />
 @endsection
