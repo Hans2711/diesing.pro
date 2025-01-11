96b73958f4eeefdc56658df39cb49580cc48776d
Hans Peter Diesing <hans.diesing@netigo.de>
2025-01-11 15:34:32 +0100

    Fix

--- app/Livewire/ContactForm.php
+++ app/Livewire/ContactForm.php
@@ -35,19 +35,6 @@ class ContactForm extends Component
         "recepient" => "required|in:hp,detlef",
     ];
 
-    protected $messages = [
-        "name.required" => "Bitte geben Sie Ihren Namen ein.",
-        "name.min" => "Ihr Name muss mindestens 3 Zeichen lang sein.",
-        "email.required" => "Bitte geben Sie Ihre E-Mail-Adresse ein.",
-        "email.email" => "Bitte geben Sie eine gültige E-Mail-Adresse ein.",
-        "tel.required" => "Bitte geben Sie Ihre Telefonnummer ein.",
-        "tel.regex" => "Bitte geben Sie eine gültige Telefonnummer ein.",
-        "message.required" => "Bitte geben Sie eine Nachricht ein.",
-        "message.min" => "Ihre Nachricht muss mindestens 10 Zeichen lang sein.",
-        "recepient.required" => "Bitte wählen Sie einen Empfänger aus.",
-        "recepient.in" => "Der ausgewählte Empfänger ist ungültig.",
-    ];
-
     public function submit()
     {
         $validatedData = $this->validate();
