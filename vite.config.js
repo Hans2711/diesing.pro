e16e52be6c972a0b07abd80721c31ac18e1aeaea
Hans Peter Diesing <hans.diesing@netigo.de>
2025-03-09 00:16:28 +0100

    Fix Certificates

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 "resources/js/parts/header.js",
                 "resources/js/utils/clipboard.js",
+                "resources/js/utils/editor.js",
             ],
             refresh: true,
         }),
