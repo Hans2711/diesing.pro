618ad8e317e7a446d4b9fd4136d0884a7c5f3ba3
Hans Peter Diesing <hans.diesing@netigo.de>
2025-03-15 16:07:47 +0100

    Loads of fixes

--- resources/views/global/header.blade.php
+++ resources/views/global/header.blade.php
@@ -1,107 +1,108 @@
 <nav class="p-5 bg-white fixed z-30 w-full shadow md:flex md:items-center md:justify-between">
-  <div class="flex justify-between items-center">
-    <span class="text-2xl font-[Poppins] cursor-pointer">
-      <a wire:navigate.hover href="{{ url('/' . Config::get('app.locale')) }}">
-        <img class="h-10 inline" src="{{ Vite::asset('resources/logo/DLogo.png') }}">
-      </a>
-    </span>
-    <div class="flex items-center z-50">
-      <div class="relative group inline-block text-left ml-4">
-        <button
-          type="button"
-          class="inline-flex items-center justify-between w-40 rounded-md border border-gray-300 px-4 py-2 bg-white text-sm font-medium "
-          id="language-button"
-          onclick="window.triggerLanguageDropdown(this)"
-        >
-          <img
-            src="{{ Vite::asset('resources/icons/' . __('language.svg') . '.svg') }}"
-            alt="{{ __('language.name') }}"
-            class="h-5 w-5 mr-2"
-          />
-          {{ __('language.name') }}
-          <img
-            src="{{ Vite::asset('resources/icons/chevron-down.svg') }}"
-            alt="Chevron"
-            class="ml-2 h-5 w-5 text-gray-400"
-          />
-        </button>
-        @php
-            $otherlang = Config::get('app.locale') == 'de' ? 'en' : 'de';
-        @endphp
-        <div
-            id="language-dropdown"
-          class="origin-top-right absolute left-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden"
-        >
-          <div class=" inline-flex justify-between w-full text-sm font-medium">
-            <a
-              href="{{ url('/' . $otherlang) }}"
-              class="flex justify-between items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              <img
-                src="{{ Vite::asset('resources/icons/' . __('language.svg-' . $otherlang) . '.svg') }}"
-                alt="{{ __('language.name-' . $otherlang) }}"
-                class="h-5 w-5 mr-2"
-              />
-              {{ __('language.name-' . $otherlang) }}
-              <img
-                src="{{ Vite::asset('resources/icons/chevron-forward.svg') }}"
-                alt="Chevron"
-                class="ml-2 h-5 w-5 text-gray-400"
-              />
+    <div class="flex justify-between items-center">
+        <span class="text-2xl font-[Poppins] cursor-pointer">
+            <a wire:navigate.hover href="{{ url('/' . Config::get('app.locale')) }}">
+                <img class="h-10 inline" src="{{ Vite::asset('resources/logo/DLogo.png') }}">
             </a>
-          </div>
+        </span>
+        <div class="flex items-center z-50">
+            <div class="relative group inline-block text-left ml-4">
+                <button
+                    type="button"
+                    class="inline-flex items-center justify-between w-40 rounded-md border border-gray-300 px-4 py-2 bg-white text-sm font-medium "
+                    id="language-button"
+                    onclick="window.triggerLanguageDropdown(this)"
+                >
+                    <img
+                        src="{{ Vite::asset('resources/icons/' . __('language.svg') . '.svg') }}"
+                        alt="{{ __('language.name') }}"
+                        class="h-5 w-5 mr-2"
+                        loading="eager"
+                    />
+                    {{ __('language.name') }}
+                    <img
+                        src="{{ Vite::asset('resources/icons/chevron-down.svg') }}"
+                        alt="Chevron"
+                        class="ml-2 h-5 w-5 text-gray-400"
+                    />
+                </button>
+                @php
+                $otherlang = Config::get('app.locale') == 'de' ? 'en' : 'de';
+                @endphp
+                <div
+                    id="language-dropdown"
+                    class="origin-top-right absolute left-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden"
+                >
+                    <div class=" inline-flex justify-between w-full text-sm font-medium">
+                        <a
+                            href="{{ url('/' . $otherlang) }}"
+                            class="flex justify-between items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 btn-language"
+                        >
+                            <img
+                                src="{{ Vite::asset('resources/icons/' . __('language.svg-' . $otherlang) . '.svg') }}"
+                                alt="{{ __('language.name-' . $otherlang) }}"
+                                class="h-5 w-5 mr-2"
+                            />
+                            {{ __('language.name-' . $otherlang) }}
+                            <img
+                                src="{{ Vite::asset('resources/icons/chevron-forward.svg') }}"
+                                alt="Chevron"
+                                class="ml-2 h-5 w-5 text-gray-400"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <span class="text-3xl z-50 cursor-pointer mx-2 md:hidden block">
+                <img class="h-9 w-9" id="menu-img" src="{{ Vite::asset('resources/icons/menu.svg') }}">
+            </span>
         </div>
-      </div>
-      <span class="text-3xl z-50 cursor-pointer mx-2 md:hidden block">
-        <img class="h-9 w-9" id="menu-img" src="{{ Vite::asset('resources/icons/menu.svg') }}">
-      </span>
     </div>
-  </div>
-  <ul id="header-list"
-      class="md:flex md:items-center z-40 md:z-auto md:static absolute bg-white w-full left-0 md:w-auto
-             md:py-0 md:pl-0 pl-3 md:opacity-100 opacity-0 top-[-400px]
-             transition-all ease-in duration-500">
-    <li class="mx-2 my-6 md:my-0">
-      <a wire:navigate.hover
-         class="@if ($active == 'portfolio') active @endif header-button"
-         href="{{ url(Config::get('app.locale') . '/' . __('url.portfolio')) }}">
-        {{ __('text.portfolio') }}
-      </a>
-    </li>
-    <li class="mx-2 my-6 md:my-0">
-      <a wire:navigate.hover
-         class="@if ($active == 'contact') active @endif header-button"
-         href="{{ url(Config::get('app.locale') . '/' . __('url.contact')) }}">
-        {{ __('text.contact') }}
-      </a>
-    </li>
-    <li class="mx-2 my-6 md:my-0 md:hidden">
-      <a wire:navigate.hover
-         class="@if ($active == 'tester') active @endif header-button"
-         href="{{ url(Config::get('app.locale') . '/' . __('url.tester')) }}">
-        {{ __('text.tester') }}
-      </a>
-    </li>
-    <li class="mx-2 my-6 md:my-0">
-      <a wire:navigate.hover
-         class="@if ($active == 'teams') active @endif header-button"
-         href="{{ url(Config::get('app.locale') . '/' . __('url.teams')) }}">
-        {{ __('text.random-teams') }}
-      </a>
-    </li>
-    <li class="mx-2 my-6 md:my-0">
-          <a
-             class="header-button"
-             href="http://www.diesing.pro:8096/">Jellyfin</a>
+    <ul id="header-list"
+        class="md:flex md:items-center z-40 md:z-auto md:static absolute bg-white w-full left-0 md:w-auto
+        md:py-0 md:pl-0 pl-3 md:opacity-100 opacity-0 top-[-400px]
+        transition-all ease-in duration-500">
+        <li class="mx-2 my-6 md:my-0">
+            <a wire:navigate.hover
+                class="@if ($active == 'portfolio') active @endif header-button"
+                href="{{ url(Config::get('app.locale') . '/' . __('url.portfolio')) }}">
+                {{ __('text.portfolio') }}
+            </a>
         </li>
-    <li class="mx-2 my-6 md:my-0">
-      <a
-         class="@if ($active == 'account') active @endif header-button"
-         href="{{ url(Config::get('app.locale') . '/' . __('url.account')) }}">
-        {{ __('text.account') }}
-      </a>
-    </li>
-  </ul>
+        <li class="mx-2 my-6 md:my-0">
+            <a wire:navigate.hover
+                class="@if ($active == 'contact') active @endif header-button"
+                href="{{ url(Config::get('app.locale') . '/' . __('url.contact')) }}">
+                {{ __('text.contact') }}
+            </a>
+        </li>
+        <li class="mx-2 my-6 md:my-0 md:hidden lg:block">
+            <a wire:navigate.hover
+                class="@if ($active == 'tester') active @endif header-button"
+                href="{{ url(Config::get('app.locale') . '/' . __('url.tester')) }}">
+                {{ __('text.tester') }}
+            </a>
+        </li>
+        <li class="mx-2 my-6 md:my-0">
+            <a wire:navigate.hover
+                class="@if ($active == 'teams') active @endif header-button"
+                href="{{ url(Config::get('app.locale') . '/' . __('url.teams')) }}">
+                {{ __('text.random-teams') }}
+            </a>
+        </li>
+        <li class="mx-2 my-6 md:my-0 sm:hidden md:block">
+            <a
+                class="header-button"
+                href="http://www.diesing.pro:8096/">Jellyfin</a>
+        </li>
+        <li class="mx-2 my-6 md:my-0">
+            <a
+                class="@if ($active == 'account') active @endif header-button"
+                href="{{ url(Config::get('app.locale') . '/' . __('url.account')) }}">
+                {{ __('text.account') }}
+            </a>
+        </li>
+    </ul>
 </nav>
 
 <div class="hidden top-[80px] opacity-100"></div>
--- resources/views/livewire/portfolio.blade.php
+++ resources/views/livewire/portfolio.blade.php
@@ -1,37 +1,53 @@
 <div>
-    <div class="flex flex-wrap gap-3 mb-5">
-        @foreach ($users as $user)
+
+    <div class="md:flex">
+        <ul class="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+            @foreach ($users as $user)
             @if ($user->portfolios->count() > 0)
-                <button class="header-button @if (($selectedUser ? $selectedUser->id : null) == $user->id) active @endif" id="{{ $user->id }}" wire:click="selectUser($event.target.id)">{{ $user->name }}</button>
+            <li>
+                @if (($selectedUser ? $selectedUser->id : null) == $user->id)
+                <a href="#" class="px-4 py-3 rounded-lg w-full ibtn active" aria-current="page">
+                    {{ $user->name }}
+                </a>
+                @else
+                <a href="#" class="px-4 py-3 rounded-lg w-full ibtn" wire:click="selectUser($event.target.id)" id="{{ $user->id }}" >
+                    {{ $user->name }}
+                </a>
+                @endif
+            </li>
             @endif
-        @endforeach
-    </div>
-
-    <div class="mb-5">
-        @if ($selectedUser)
-            <a wire:navigate.hover class="btn w-fit" href="{{ url(Config::get('app.locale') . '/' . __('url.contact')) }}/{{ $selectedUser->email }}" >{{ __('text.contact') }}</a>
-        @endif
-    </div>
-
-    <div class="md:columns-2 columns-1 mb-8" id="portfolio">
-        @if ($portfolios)
-            @foreach ($portfolios as $portfolio)
-                @foreach ($portfolio as $item)
-                    <div class="max-w-lg rounded overflow-hidden shadow-lg mt-4 md:mt-0">
-                        <a href="{{$item->url}}" target="_blank">
-                            @if ($item->media)
-                                <div class="relative h-0 pb-56">
-                                    <img class="absolute top-0 left-0 w-full h-full object-cover" src="{{ Storage::url($item->media[0]->path) }}">
-                                </div>
-                            @endif
-                            <div class="px-6 py-4">
-                                <div class="font-bold text-xl mb-2">{{$item->name}}</div>
-                                <div class="text-gray-700 text-base break-words">{!! $item->description !!}</div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
             @endforeach
-        @endif
+        </ul>
+        <div class="md:px-6 md:pt-0 text-medium rounded-lg w-full">
+            <div class="space-y-6 mb-8 md:columns-2" id="portfolio">
+                @if ($portfolios)
+                @foreach ($portfolios as $portfolio)
+                @foreach ($portfolio as $item)
+                <div class="max-w-lg w-full rounded overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+                    <a href="{{ $item->url }}" target="_blank">
+                        @if ($item->media)
+                        <div class="relative h-0 pb-56 overflow-hidden">
+                            <img class="absolute inset-0 w-full h-full object-cover" loading="lazy" src="{{ Storage::url($item->media[0]->path) }}">
+                        </div>
+                        @endif
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">{{ $item->name }}</div>
+                            <div class="text-gray-700 text-base break-words">{!! $item->description !!}</div>
+                        </div>
+                    </a>
+                </div>
+                @endforeach
+                @endforeach
+                @endif
+            </div>
+
+            <div class="mb-5">
+                @if ($selectedUser)
+                <a wire:navigate.hover class="btn w-fit" href="{{ url(Config::get('app.locale') . '/' . __('url.contact')) }}/{{ $selectedUser->email }}" >{{ __('text.contact') }}</a>
+                @endif
+            </div>
+        </div>
     </div>
+
+
 </div>
