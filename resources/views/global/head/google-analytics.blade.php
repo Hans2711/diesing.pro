02f6da6103f0c431a140befec078b264ffee652d
Hans Peter Diesing <hans.diesing@netigo.de>
2025-03-22 15:13:21 +0100

    bla

--- resources/views/global/head/google-analytics.blade.php
+++ resources/views/global/head/google-analytics.blade.php
@@ -1,6 +1,6 @@
 @if (env('APP_ANALYTICS'))
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SX1DCPHNNB"></script>
+    <script defer src="https://www.googletagmanager.com/gtag/js?id=G-SX1DCPHNNB"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
