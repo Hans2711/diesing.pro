3fb5380e4546c1c075e7c25b0320240e94eb13d5
Hans Peter Diesing <hans.diesing@netigo.de>
2025-04-23 09:30:58 +0200

    Fix

--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1 class="">{{ __('text.cv') }}</h1>
 
-    <a wire:navigate href="{{ url(Config::get('app.locale') . '/' . __('url.cv')) }}/hp@diesing.pro" class="btn w-fit">Hans Peter (HP) Diesing {{ __('text.cv')}}</a>
+    <a wire:navigate.hover href="{{ url(Config::get('app.locale') . '/' . __('url.cv')) }}/hp@diesing.pro" class="btn w-fit">Hans Peter (HP) Diesing {{ __('text.cv')}}</a>
 @endsection
 
