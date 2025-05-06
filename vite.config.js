7672196b0f72a378bd7042a93a2f4ac2514e69a1
Hans Peter Diesing <hans.diesing@netigo.de>
2025-05-06 17:33:51 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 "resources/js/utils/clipboard.js",
                 "resources/js/utils/editor.js",
+                "resources/js/utils/zenquotes.js",
             ],
             refresh: true,
         }),
