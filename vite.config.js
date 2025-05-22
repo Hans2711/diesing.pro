0fb581229776cd690ae217862663f8551e534b73
Hans Peter Diesing <hans.diesing@netigo.de>
2025-05-22 18:28:51 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import obfuscator from "vite-plugin-javascript-obfuscator";
 import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
+    build: {
+        manifest: true,
+    },
     plugins: [
         laravel({
             input: [
