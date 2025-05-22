1e522981c97b99874b8595fd4a8321a90cd022cf
Hans Peter Diesing <hp@diesing.pro>
2025-05-22 18:13:16 +0200

    Integrate asset obfuscation, compression and HTML minification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(SetLocale::class);
+        $middleware->append(MinifyHtmlMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {})
     ->create();
