424eda9af1f1641fcceadd51476b02e634827267
Hans Peter Diesing <hans.diesing@netigo.de>
2025-05-22 18:36:55 +0200

    Fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,22 @@ const images = import.meta.glob(
   }
 );
 
+const logo = import.meta.glob(
+  '../logo/*.{png,jpg,jpeg,svg,gif,webp}',
+  {
+    eager: true,
+    import: 'default',
+  }
+);
+
+const icons = import.meta.glob(
+  '../icons/*.svg',
+  {
+    eager: true,
+    import: 'default',
+  }
+);
+
 import.meta.glob([
     "../logo/**",
     "../portfolio/**",
