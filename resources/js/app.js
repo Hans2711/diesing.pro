681fb36c0aa999af938ed984ae85b0e95e96f280
Hans Peter Diesing <hans.diesing@netigo.de>
2025-05-22 18:34:09 +0200

    Fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import "./bootstrap";
 import "../css/app.css";
 
+const images = import.meta.glob(
+  '../images/*.{png,jpg,jpeg,svg,gif,webp}',
+  {
+    eager: true,
+    import: 'default',
+  }
+);
+
 import.meta.glob([
     "../logo/**",
     "../portfolio/**",
