cc00c7731625e462fa195bd3c4808a8f8e622e31
Hans Peter Diesing <hans.diesing@netigo.de>
2025-05-26 15:32:40 +0200

    Add 'activeTool' => 'tester' to Blade templates for consistent navigation context.

--- resources/views/tester/testrun.blade.php
+++ resources/views/tester/testrun.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['title' => __('text.testrun') . ' ' . $testrun->name . ' ' . $testrun->created_at_clean, 'active' => 'tester'])
+@extends('layouts.app', ['title' => __('text.testrun') . ' ' . $testrun->name . ' ' . $testrun->created_at_clean, 'active' => 'tester', 'activeTool' => 'tester'])
 
 @section('content')
     <h1>{{ __('text.testrun') }} {{$testrun->name}} {{$testrun->created_at_clean}}</h1>
