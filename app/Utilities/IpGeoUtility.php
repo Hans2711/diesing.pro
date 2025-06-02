0cb2cdeb323c6fb27a5da0d29272b85ea91c6d06
Hans Peter Diesing <hp@diesing.pro>
2025-06-02 19:39:17 +0200

    Add caching to IpGeoUtility

--- app/Utilities/IpGeoUtility.php
+++ app/Utilities/IpGeoUtility.php
@@ -1,14 +1,17 @@
 <?php
 namespace App\Utilities;
 
+use Illuminate\Support\Facades\Cache;
+
 class IpGeoUtility
 {
-    public static function getGeo($ip) {
-        $url = "http://ip-api.com/json/$ip";
-        $response = file_get_contents($url);
-        $data = json_decode($response);
-        return $data;
+    public static function getGeo($ip)
+    {
+        return Cache::rememberForever("ip-geo-{$ip}", function () use ($ip) {
+            $url = "http://ip-api.com/json/{$ip}";
+            $response = file_get_contents($url);
+            return json_decode($response);
+        });
     }
-
 }
 
