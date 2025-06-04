6d519670a9db3707ad449530dc9a1e0fae6175db
Hans Peter Diesing <hans.diesing@netigo.de>
2025-06-04 14:33:20 +0200

    Fix

--- resources/views/livewire/rss-feeds.blade.php
+++ resources/views/livewire/rss-feeds.blade.php
@@ -4,35 +4,35 @@
     </button>
     <div class="mt-3 w-full">
         @foreach ($feeds ?? [] as $feed)
-            <div class="p-3 mt-5 rounded border-solid border w-full" style="border-color: #6b7280;">
-                @if (isset($selectedFeed['id']) && $feed->id == $selectedFeed['id'])
-                    <form wire:submit.prevent="updateFeed">
-                        <label for="edit-url" class="font-bold text-lg">URL</label>
-                        <input id="edit-url" type="text" wire:model="selectedFeed.url" class="w-full p-2 border rounded dark:bg-secondary-light" />
-                        <div class="flex justify-end gap-3 mt-3">
-                            <button type="button" wire:click="cancelEdit" class="btn btn-delete">
-                                <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/close.svg') }}" alt="{{ __('alt.close') }}" title="{{ __('alt.close') }}"/>
-                            </button>
-                            <button type="submit" class="btn btn-fetch">
-                                <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/save-outline.svg') }}" alt="{{ __('alt.save') }}" title="{{ __('alt.save') }}"/>
-                            </button>
-                        </div>
-                    </form>
-                @else
-                    <div class="flex justify-between">
-                        <span class="break-all">{{ $feed->url }}</span>
-                        <span class="text-sm text-gray-500">{{ $feed->updated_at->format('d.m.Y H:i') }}</span>
-                    </div>
-                    <div class="grid grid-cols-2 gap-5 mt-2">
-                        <button wire:click="deleteFeed({{ $feed->id }})" wire:confirm="{{ __('text.are-you-sure') }}" class="btn btn-delete">
-                            <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/close.svg') }}" alt="{{ __('alt.close') }}" title="{{ __('alt.close') }}"/>
-                        </button>
-                        <button wire:click="editFeed({{ $feed->id }})" class="btn btn-edit">
-                            <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/pencil-outline.svg') }}" alt="{{ __('alt.edit') }}" title="{{ __('alt.edit') }}"/>
-                        </button>
-                    </div>
-                @endif
+        <div class="p-3 mt-5 rounded border-solid border w-full" style="border-color: #6b7280;">
+            @if (isset($selectedFeed['id']) && $feed->id == $selectedFeed['id'])
+            <form wire:submit.prevent="updateFeed">
+                <label for="edit-url" class="font-bold text-lg">URL</label>
+                <input id="edit-url" type="text" wire:model="selectedFeed.url" class="w-full p-2 border rounded dark:bg-secondary-light" />
+                <div class="flex justify-end gap-3 mt-3">
+                    <button type="button" wire:click="cancelEdit" class="btn btn-delete">
+                        <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/close.svg') }}" alt="{{ __('alt.close') }}" title="{{ __('alt.close') }}"/>
+                    </button>
+                    <button type="submit" class="btn btn-fetch">
+                        <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/save-outline.svg') }}" alt="{{ __('alt.save') }}" title="{{ __('alt.save') }}"/>
+                    </button>
+                </div>
+            </form>
+            @else
+            <div class="flex justify-between">
+                <span class="break-all">{{ $feed->url }}</span>
+                <span class="text-sm text-gray-500">{{ $feed->updated_at->format('d.m.Y H:i') }}</span>
             </div>
+            <div class="grid grid-cols-2 gap-5 mt-2">
+                <button id="{{ $feed->id }}" wire:click="deleteFeed($event.target.id)" class="btn btn-delete" type="button" wire:confirm="{{ __('text.are-you-sure') }}">
+                    <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/close.svg') }}" alt="{{ __('alt.close') }}" title="{{ __('alt.close') }}"/>
+                </button>
+                <button wire:click="editFeed({{ $feed->id }})" class="btn btn-edit">
+                    <img class="w-10 h-5 invert" src="{{ Vite::asset('resources/icons/pencil-outline.svg') }}" alt="{{ __('alt.edit') }}" title="{{ __('alt.edit') }}"/>
+                </button>
+            </div>
+            @endif
+        </div>
         @endforeach
     </div>
 </div>
