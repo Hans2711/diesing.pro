40a8686cae2f486403b2e41a20e4bc68e4accd61
Hans Peter Diesing <hp@diesing.pro>
2025-06-07 00:33:51 +0200

    Add from parameter support across account tools

--- app/Http/Middleware/RedirectsMiddleware.php
+++ app/Http/Middleware/RedirectsMiddleware.php
@@ -33,7 +33,8 @@ class RedirectsMiddleware
             "/" .
             __("url.account") .
             "?return_url=" .
-            urlencode($currentUrl);
+            urlencode($currentUrl) .
+            "&from=redirects";
 
         return redirect($redirectUrl);
     }
